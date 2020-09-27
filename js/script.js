ba516e65d70ae4e5b46a4a7fbd1ab7d0d95c0972
Alex Chernyak <Fugerson91@gmail.com>
2020-09-27 23:00:31 +0300

    fint commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// $(document).ready(function() {
+//     $('real__container')
+// })
+$(document).ready(function() {
+    $('.real__container').slick();
+});
+
+
+// addEventListener("load", () => {
+//     let projectsIcon = document.querySelectorAll(".priject__icon-link2")
+//     let projectsItemImg = document.querySelectorAll(".projects-img")
+
+//     for (let i = 0; i < projectsIcon.length; i++) {
+//         projectsIcon[i].addEventListener("click", function projectClick() {
+//             // console.dir(projectsItem[i])
+//             projectsItemImg[i].classList.add("project__show")
+
+//         })
+//     }
+// })
