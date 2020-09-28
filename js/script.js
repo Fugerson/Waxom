49f5298d076f8b4c13ae230cea0748f9eaffbb29
Alex Chernyak <Fugerson91@gmail.com>
2020-09-28 16:38:33 +0300

    publication

--- js/script.js
+++ js/script.js
@@ -18,3 +18,19 @@ $(document).ready(function() {
 //         })
 //     }
 // })
+let menuBlock = document.querySelector(".minWidth-menu");
+
+let menuUl = document.querySelector(".to-be-changed");
+
+menuBlock.addEventListener("click", (event) => {
+    event.stopPropagation()
+
+    menuUl.style.display = "block"
+})
+let body = document.querySelector("body")
+
+addEventListener("click", (event) => {
+    event.stopPropagation()
+
+    menuUl.style.display = "none"
+})
